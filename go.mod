6a25e30394dcd3fd74522e4ec9ad3994d52f37c5
gscyber <amartin@gscapts.com>
2024-06-27 20:49:28 +0000

    dash

--- go.mod
+++ go.mod
@@ -2,9 +2,4 @@ module github.com/gscyber/Intern_Project
 
 go 1.22.3
 
-require (
-	github.com/gorilla/sessions v1.3.0
-	github.com/lib/pq v1.10.9
-)
-
-require github.com/gorilla/securecookie v1.1.2 // indirect
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -3,7 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
+	"database/sql"
+	"os"
+	_ "github.com/lib/pq"
 )
+
+
+var db *sql.DB
+var tmpl = template.Must(template.ParseFiles("templates/index.html"))
+
+func initDB() {
+	var err error
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Printf("Error opening database: %q", err)
+	}
+	if err = db.Ping(); err != nil {
+		fmt.Printf("Error connecting to the database: %q", err)
+	}
+	fmt.Println("Successfully connected to the database")
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    tmpl.Execute(w, map[string]interface{}{
+    })
+}
+
 
 func main() {
 	// initialize database
@@ -11,8 +37,6 @@ func main() {
 	defer db.Close()
 
 	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/login", loginHandler)
-    http.HandleFunc("/logout", logoutHandler)
 	
 	fmt.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
